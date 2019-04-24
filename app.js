82f288b9d7e7623378ea78e5fa9a242fd458141b
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2019-04-24 23:23:18 +0200

    Two ways binding

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 new Vue({
   el:'#vue-app' ,
   data: {
-
+    name:'',
+    age:''
   },
   methods:{
     logName: function(){
-      console.log('You entered your name');
+      console.log('You entered your age');
     },
     logAge: function(){
       console.log('You entered your age');
