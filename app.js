bfbc697066ec616ae3877e90b3c17ac49a957349
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2019-04-24 22:44:31 +0200

    Add and subtract button to modify the data

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 new Vue({
   el:'#vue-app' ,
   data: {
-    name: 'Shaun',
-    job: 'Ninja',
-    website:'http://www.thenetninja.co.uk',
-    websiteTag:'<a href="http://www.thenetninja.co.uk">The Net Ninja Website</a>'
+    age:25
   },
   methods:{
-    greet: function(time){
-      return 'Good '+ time + ' '+this.name;
-    }
+
   }
 });
