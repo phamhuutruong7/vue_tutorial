e1fb64e74eab2fdf20ab70a0a6419eeae7fbad7b
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2019-05-15 00:06:27 +0200

    This is how to use 1 property to create attribute to pass data.

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
       <app-header></app-header>
-      <app-ninjas></app-ninjas>
+      <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
       <app-footer></app-footer>
   </div>
 </template>
@@ -9,7 +9,7 @@
 <script>
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
-import Ninjas from './components/Ninjas.vue'
+import Ninjas from './components/Ninjas.vue';
 
 export default {
     components:{
@@ -19,7 +19,14 @@ export default {
     },
     data() {
       return{
-
+        ninjas:[
+          {name:'Ryu', speciality:'Vue Components', show:false},
+          {name:'Crystal', speciality:'HTML Wizardry', show:false},
+          {name:'Hitoshi', speciality:'Click Events', show:false},
+          {name:'Tango', speciality:'Conditionals', show:false},
+          {name:'Kami', speciality:'Webpack', show:false},
+          {name:'Yoshi', speciality:'Data Diggin', show:false}
+        ]
       }
     }
 }
